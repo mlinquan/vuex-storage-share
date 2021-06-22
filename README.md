1e6cb331b24ab29fabf48021522db38dbb7b1fbd
林泉 <linquan@bailongma-inc.com>
2021-06-22 14:41:41 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# vuex-storage-share
+[![npm version](https://badge.fury.io/js/vuex-storage-share.svg)](https://badge.fury.io/js/vuex-storage-share)
+[![Gzip Size](http://img.badgesize.io/https://unpkg.com/vuex-storage-share@latest/dist/vuex-storage-share.umd.min.js?compression=gzip&style=flat-square)](https://unpkg.com/vuex-storage-share)
+[![Monthly Downloads](https://img.shields.io/npm/dm/vuex-storage-share.svg)](https://www.npmjs.com/package/vuex-storage-share)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Installation
+
+Install
+
+```sh
+$ npm install vuex-storage-share --save
+# or
+$ yarn add vuex-storage-share
+```
+
+## Usage
+
+```js
+
+import VuexStorageShare from 'vuex-storage-share'
+
+const storagePlugin = VuexStorageShare({
+  storagePrefix: 'VUEX_STORAGE/',
+  predicate: ['SET_USER_INFO', 'SET_TOKEN', 'SET_USER_AVATAR'],
+  expires_in: 360, // one hour
+  storageListener({ type, payload }) {
+    const route = router.app._route
+    if (type === 'SET_USER_INFO') {
+      if (!(payload && payload.is_login) && route.path !== '/login') {
+        router.replace({
+          path: '/login',
+          query: {
+            redirect: route.fullPath
+          }
+        })
+      }
+      if ((payload && payload.is_login) && route.path === '/login') {
+        router.replace(route.query && route.query.redirect || '/home')
+      }
+    }
+  }
+})
+
+const store = new Vuex.Store({
+  state: {
+    userinfo: {
+      is_login: false
+    }
+  },
+  mutations: {
+    SET_USER_INFO(state, userinfo) {
+      userinfo.is_login = !!userinfo.id
+      state.userinfo = userinfo
+    }
+  },
+  modules: {
+    app,
+    user,
+  },
+  getters,
+  plugins: [
+    storagePlugin.subscriber
+  ]
+})
+```
+
+## Options
+
+| Option | Explain | Type | Default | 
+| ------ | ------ | ------ | ------ |
+| storagePrefix | localStorage key prefix. It is also a unique identification. | `String` | `VUEX_STORAGE/` |
+| expires_in | Expiration time in seconds. | `Number` | `0` |
+| predicate | List of mutations to store and share. | `Array` | `[]` |
+| storageListener | `storageListener({ type, payload }) {}` Whenever the store is persisted, return a mutation object. | `Function` | `-` |
+
+### MIT License
+
+### Copyright &copy; 2021 LinQuan.
---
+++ src/vuex-storage-share.js
@@ -0,0 +1,142 @@
+const StoragePluginCache = {}
+
+const vuexStorageShare = function(storagePrefix, options) {
+  if (typeof options !== 'object') {
+    options = {}
+  }
+  if (typeof storagePrefix === 'object') {
+    options = Object.assign(storagePrefix, options)
+  }
+  if (typeof storagePrefix === 'string') {
+    options.storagePrefix = storagePrefix
+  }
+  options = Object.assign(
+    {
+      // cache_time: 24
+      expires_in: 360,
+      storagePrefix: 'VUEX_STORAGE/',
+      predicate: () => {
+        return true
+      }
+    },
+    options
+  )
+  if(!StoragePluginCache[options.storagePrefix]) {
+  	StoragePluginCache[options.storagePrefix] = new StoragePlugin(options)
+  }
+  return StoragePluginCache[options.storagePrefix]
+}
+
+const StoragePlugin = function(options) {
+  this.initStatus = false
+  this.storagePrefix = options.storagePrefix
+  this.expires_in = options.expires_in
+
+  if (Array.isArray(options.predicate)) {
+    const predicate = options.predicate
+    if (predicate.length) {
+      options.predicate = (mutation) => {
+        return predicate.includes(mutation.type)
+      }
+    }
+    if (!predicate.length) {
+      options.predicate = (mutation) => {
+        return true
+      }
+    }
+  }
+
+  const storageHandler = (e) => {
+    if (!e.key || !this.initStatus || e.key.indexOf(this.storagePrefix) !== 0) {
+      return
+    }
+
+    const type = e.key.substring(this.storagePrefix.length)
+    let payload = e.newValue
+    try {
+      payload = JSON.parse(e.newValue)
+    } catch (e) {
+      console.log(e)
+    }
+
+    if (typeof options.storageListener === 'function') {
+      options.storageListener.call(null, {
+        type,
+        payload
+      })
+    }
+  }
+
+  window.addEventListener('storage', storageHandler, false)
+
+  this.subscriber = store => {
+    // 从 localStorage 获取 state 进行初始化
+    Object.keys(window.localStorage).forEach(key => {
+      if (key.indexOf(this.storagePrefix) !== 0) {
+        return
+      }
+      key = key.substring(this.storagePrefix.length)
+      if (!options.predicate({
+        type: key
+      })) {
+        return
+      }
+      try {
+        store.commit(key, this.get(key))
+      } catch (e) {
+        console.log(e)
+      }
+    })
+    this.initStatus = true
+    // 当 store 初始化后调用
+    store.subscribe((mutation, state) => {
+      // 每次 mutation 之后调用
+      // mutation 的格式为 { type, payload }
+      if (options.predicate(mutation, state)) {
+        this.set(mutation.type, mutation.payload)
+      }
+    })
+  }
+}
+
+StoragePlugin.prototype.set = function(key, data) {
+  key = this.storagePrefix + key
+  try {
+    data = JSON.stringify(data)
+  } catch (e) {
+    console.log(e)
+  }
+  window.localStorage.setItem(key, data)
+}
+
+StoragePlugin.prototype.get = function(key) {
+  key = this.storagePrefix + key
+  let data = window.localStorage.getItem(key)
+  try {
+    data = JSON.parse(data)
+  } catch (e) {
+    console.log(e)
+  }
+  return data
+}
+
+StoragePlugin.prototype.remove = function(key) {
+  key = this.storagePrefix + key
+  try {
+    window.localStorage.removeItem(key)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+StoragePlugin.prototype.clear = function() {
+  Object.keys(window.localStorage).forEach(key => {
+    if (key.indexOf(this.storagePrefix) !== 0) {
+      return
+    }
+    key = key.substring(this.storagePrefix.length)
+    this.remove(key)
+  })
+}
+
+export default vuexStorageShare
